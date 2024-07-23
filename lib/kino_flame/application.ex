adc848d9c5a4047bec1f1fb736edd3076325401b
Jonatan Kłosko <jonatanklosko@gmail.com>
2024-07-23 22:30:43 +0700

    Initial commit

---
+++ lib/kino_flame/application.ex
@@ -0,0 +1,12 @@
+defmodule KinoFLAME.Application do
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = []
+    opts = [strategy: :one_for_one, name: KinoFLAME.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,51 @@
+defmodule KinoFLAME.MixProject do
+  use Mix.Project
+
+  @version "0.1.0"
+  @description "FLAME integration with Livebook"
+
+  def project do
+    [
+      app: :kino_flame,
+      version: @version,
+      description: @description,
+      name: "KinoFLAME",
+      elixir: "~> 1.16",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      docs: docs(),
+      package: package()
+    ]
+  end
+
+  def application do
+    [
+      mod: {KinoFLAME.Application, []}
+    ]
+  end
+
+  defp deps do
+    [
+      {:kino, "~> 0.13"},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "components",
+      source_url: "https://github.com/livebook-dev/kino_flame",
+      source_ref: "v#{@version}",
+      extras: ["guides/components.livemd"]
+    ]
+  end
+
+  def package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/livebook-dev/kino_flame"
+      }
+    ]
+  end
+end
