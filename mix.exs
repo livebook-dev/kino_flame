7a15bd07e8b75d3be93108a937a87836ffb06fc4
Jonatan Kłosko <jonatanklosko@gmail.com>
2024-07-24 17:59:42 +0700

    Up

--- mix.exs
+++ mix.exs
@@ -27,8 +27,8 @@ defmodule KinoFLAME.MixProject do
   defp deps do
     [
       {:kino, "~> 0.13"},
-      # TODO bump once the next version is released
-      {:flame, "~> 0.2.0"},
+      # TODO: bump once the next version is released
+      {:flame, github: "phoenixframework/flame"},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false}
     ]
   end
